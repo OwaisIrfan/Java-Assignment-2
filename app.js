dadedb019e103c14edc09f6866a091b6cbe47a1b
OwaisIrfan <65021720+OwaisIrfan@users.noreply.github.com>
2020-06-21 16:40:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,194 @@
+// chapter1
+
+
+// (Task#1)
+// alert('Error! Please enter valid password')
+
+
+
+// (Task#2)
+// alert('Welcome to JS Land\nHappy Coding!')
+
+
+// (Task#3)
+// alert('Welcome to JS Land')
+// alert('Prevent this page from creating additional dialogs.')
+
+
+// (Task#4)
+// var a = ('Hello... I can run JS thorough my web browser console');
+// alert (a); 
+// This code should be paste in the console for the result
+
+
+
+
+
+// chapter2
+
+
+
+
+// (Task#1)
+// var username = task1
+
+
+// (Task#2)
+// var myName = ('john')
+
+// (Task#3)
+// var titledmessage = ('Hello World');
+// alert(titledmessage);
+
+// (Task#4)
+// var a = ('jhone doe');
+// var b = ('15 years old');
+// var c = ('Certified Mobile Application Development');
+// alert(a);
+// alert(b);
+// alert(c);
+
+// (Task#5)
+// alert(' PIZZA \n PIZZ \n PIZ \n PI \n P');
+
+// (Task#6)
+// var Email = ('My Email address is')
+// var a = ('example@example.com')
+// alert(Email + " " + a)
+
+// (Task#7)
+// var book = ('I am trying to learn from the Book A smarter way to learn JavaScript')
+// alert (book);
+
+// (Task#8)
+// var a = ('Yah! I can write HTML content through JavaScript');
+// document.write(a);
+
+// (Task#9)
+// var a =('▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬');
+// alert(a);
+// document.write(a);
+
+
+
+
+// chapter3
+ 
+// (Task#1)
+// var age = ('I am 15 years old')
+// alert(age)
+
+
+// (Task#2)
+
+// var visited =('You have visited this site 14 times')
+// alert (visited)
+
+
+// (Task#3)
+
+// var birthyear =('My birth year is 1990 Data type of my declared variable is number')
+// document.write (birthyear);
+
+
+// (Task#4)
+
+// var ordered = ('John Doe ordered 5 T-shirt(s) on XYZ Clothing store')
+// document.write (ordered)
+
+// Chapter#4
+
+// (Task#5)
+
+// var rules = ('Rules for naming JS variables')
+// var names = ('Variable names can only contain, numbers, $ and_. For example $my_1stVariable')
+// var letter = ('Variables must begin with a letter, $ or _ . For example $name, _name or name')
+// var sensitive = ('Variable names are case sensitive')
+// var keywords = ('Variable names should not be JS keywords')
+
+// document.write 
+// (rules + "<br>" + "<br>" + "<br>" + names + "<br>" + letter + "<br>" + sensitive + "<br>" + keywords);
+
+// Chapter#5
+
+// (task#1)
+
+// var First = +prompt('Enter First Value');
+// var Second = +prompt('Enter Second Value');
+// var answer = First + Second;
+// document.write ('Sum of ' + First + " " + 'and ' + Second + " " + 'is ' + answer);
+
+// (Task#2)
+//Subtraction
+// var First = +prompt('Enter First Value');
+// var Second = +prompt('Enter Second Value');
+// var answer = First - Second;
+// document.write ('Difference of ' + First + " " + 'and ' + Second + " " + 'is ' + answer);
+
+// //Multiplication
+// var First = +prompt('Enter First Value');
+// var Second = +prompt('Enter Second Value');
+// var answer = First * Second;
+// document.write ('Product of ' + First + " " + 'and ' + Second + " " + 'is ' + answer);
+
+// //Division
+// var First = +prompt('Enter First Value');
+// var Second = +prompt('Enter Second Value');
+// var answer = First / Second;
+// document.write ('Division of ' + First + " " + 'and ' + Second + " " + 'is ' + answer);
+
+//Modulus
+// var First = +prompt('Enter First Value');
+// var Second = +prompt('Enter Second Value');
+// var answer = First % Second;
+// document.write ('Modulus of ' + First + " " + 'and ' + Second + " " + 'is ' + answer);
+
+
+// (Task#3)
+
+// a) var a 
+
+// b) document.write('Value after variable declaration is ' + a )
+
+//c) var a = 5
+
+//d) document.write('Initial Value: ' + a )
+
+// e) a++
+
+//f) var a = 5
+
+//g) var increment = a+1
+// document.write('Value after increment is: ' + increment )
+
+//h) var incrementsec = increment + 7
+// document.write('Value after Addition: ' + incrementsec )
+
+// i, j) var a = 13
+// var dec = 13 - 1 
+// document.write('Value after decrement is: ' + dec )
+
+// k, l) var a = 12
+// var div = 12 % 3
+// document.write('The remainder is ' + div )
+
+
+// (Task#4)
+// var buyers = +prompt ('Total Tickets')
+// var ticket = 600
+// var answer = buyers * ticket
+// document.write('Total Cost to buy ' + buyers + ' tickets to a movie is ' + answer + ' PKR')
+
+
+// (Task#5)
+for ( i = 1; i<=10; i++){
+    document.write(i)
+}
+
+
+
+
+
+
+
+
